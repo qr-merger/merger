d8fc9d57f5e321c181b81db928d3fda7d268bb10
Focus Chen 惶心 <i@focuschen.com>
2024-04-27 14:39:05 +0100

    Update staticfile.org link and code comments, bump jQuery to 3.7.1

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,9 +1,8 @@
 
 // Merger - Merge Your Qrcode Together. Even Quicker.
-// User docs: https://merger.huangxin.org
+// Homepage: https://merger.huangxin.org
 // Released under GNU General Public License v3.0. Open source at https://github.com/qr-merger/merger.
 // Author @hifocus (https://github.com/hifocus), and contributors(https://github.com/qr-merger/merger/graphs/contributors).
-// Copyright reservation is required.
 
 var client;
 var selected;
@@ -248,7 +247,7 @@ document.getElementById("name").innerHTML = finaltitle;
 document.getElementById("description").innerHTML = finalsub;
 
 // Copyright console log copied from https://github.com/MoePlayer/APlayer/. Thank you.
-console.log(`${'\n'} %c Merger 0.28.4 %c https://github.com/qr-merger/merger ${'\n'}`, 'color: #fadfa3; background: #030307; padding:5px 0;', 'background: #fadfa3; padding:5px 0;');
+console.log(`${'\n'} %c Merger 0.28.5 %c https://github.com/qr-merger/merger ${'\n'}`, 'color: #fadfa3; background: #030307; padding:5px 0;', 'background: #fadfa3; padding:5px 0;');
 
 function showqrcode() {
   if (location.href.substr(location.href.lastIndexOf('#') + 1) == "showqrcode" && selected == "yes") document.getElementById('showqrcode').style.display = "flex";
